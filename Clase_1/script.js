b5cc75a66dfdced7e7ae9b9efb1c5cca16104ec3
Freddygrego <100003363+Freddygrego@users.noreply.github.com>
2022-10-21 18:26:06 -0500

    Oct_20

---
+++ Clase_1/script.js
@@ -0,0 +1,17 @@
+/*console.log("Javascript is amazing");/ejercicio 1
+console.log("C is fun");
+console.log("Python is cool ");*/
+// function llamar (algo){               //ejercicio 2
+//     if (algo === undefined){
+//         console.log("argument not found")
+//     }else{
+//         console.log(algo)
+//     }
+// }   
+// function llamar (param1, param2){       //ejercicio 3
+//     console.log(param1 +" " + "is" + " " + param2)
+
+// }
+function llamar (param1, param2){
+    console.log(param1)
+}
