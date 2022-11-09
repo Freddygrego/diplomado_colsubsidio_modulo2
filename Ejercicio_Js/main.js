2bdbb16362b3a67266a11fb89bd138a87a5721e8
Freddygrego <100003363+Freddygrego@users.noreply.github.com>
2022-11-08 21:01:05 -0500

    JavaScript

---
+++ Ejercicio_Js/main.js
@@ -0,0 +1,36 @@
+let variasTarjetas = 
+[
+    {
+        "contenidoTitulo": "Card 1",
+        "contenidoTexto": "Texto que debe observarse en Card 1",
+        "contenidoBoton": "Pulsame"
+    },
+    {
+        "contenidoTitulo": "Card 2",
+        "contenidoTexto": "Texto que debe observarse en Card 2",
+        "contenidoBoton": "Pulsame"
+    },
+    {
+        "contenidoTitulo": "Card 3",
+        "contenidoTexto": "Texto que debe observarse en Card 3",
+        "contenidoBoton": "Pulsame"
+    },
+    {
+        "contenidoTitulo": "Card 4",
+        "contenidoTexto": "Texto que debe observarse en Card 4",
+        "contenidoBoton": "Pulsame"
+    },
+    {
+        "contenidoTitulo": "Card 5",
+        "contenidoTexto": "Texto que debe observarse en Card 5",
+        "contenidoBoton": "Pulsame"
+    },
+    {
+        "contenidoTitulo": "Card 6",
+        "contenidoTexto": "Texto que debe observarse en Card 6",
+        "contenidoBoton": "Pulsame"
+    }
+]
+
+variasTarjetas.forEach(cicloForEach);
+
